403c23bba0ebbd41c49e286d1a41f8eb2a076635
Xintao <wxt1994@126.com>
2020-10-29 02:21:45 +0800

    update test_imresize_bicubic

--- test_imresize_bicubic.py
+++ test_imresize_bicubic.py
@@ -1,7 +1,19 @@
 import cv2
 import numpy as np
+import torch
 
 from imresize import imresize
+
+
+def imresize_np(img):
+    if type(img).__module__ == np.__name__:  # numpy type
+        numpy_type = True
+        img = torch.from_numpy(img.transpose(2, 0, 1)).float()
+    out = imresize(img)
+    if numpy_type:
+        out = out.numpy().transpose(1, 2, 0)
+    return out
+
 
 # read images
 img = cv2.imread('imresize_bicubic/baboon.png') / 255.
@@ -11,7 +23,7 @@ for mode in ['down', 'up']:
     for scale in [2, 3, 4]:
         if mode == 'down':
             resize_scale = 1 / scale
-        img_resize = imresize(img, resize_scale, antialiasing=True)
+        img_resize = imresize_np(img, resize_scale, antialiasing=True)
         img_resize = (img_resize * 255).round()
         img_resize_matlab = cv2.imread(
             f'imresize_bicubic/baboon_{mode}_x{scale}_matlab.png')
