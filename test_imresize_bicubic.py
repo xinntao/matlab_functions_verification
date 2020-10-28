9f9399f965f5eb76b0a5d1f03b7f66caffb3f421
Xintao <wxt1994@126.com>
2020-10-29 02:11:41 +0800

    update test_imresize_bicubic

--- test_imresize_bicubic.py
+++ test_imresize_bicubic.py
@@ -10,8 +10,8 @@ img = cv2.imread('imresize_bicubic/baboon.png') / 255.
 for mode in ['down', 'up']:
     for scale in [2, 3, 4]:
         if mode == 'down':
-            scale = 1 / scale
-        img_resize = imresize(img, scale, antialiasing=True)
+            resize_scale = 1 / scale
+        img_resize = imresize(img, resize_scale, antialiasing=True)
         img_resize = (img_resize * 255).round()
         img_resize_matlab = cv2.imread(
             f'imresize_bicubic/baboon_{mode}_x{scale}_matlab.png')
