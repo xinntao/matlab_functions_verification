00075cf90b03dd609136cacff034104dea1b834f
Xintao <wxt1994@126.com>
2020-10-29 02:08:45 +0800

    update test_imresize_bicubic

--- test_imresize_bicubic.py
+++ test_imresize_bicubic.py
@@ -6,32 +6,17 @@ from matlab_functions import imresize
 # read images
 img = cv2.imread('imresize_bicubic/baboon.png') / 255.
 
-# downsample x2
-img_down_x2 = imresize(img, 1 / 2, antialiasing=True)
-img_down_x2 = (img_down_x2 * 255).round()
-img_down_x2_matlab = cv2.imread('imresize_bicubic/baboon_down_x2_matlab.png')
-diff = img_down_x2 - img_down_x2_matlab
-diff_abssum = np.sum(np.abs(diff))
-h, w, _ = img_down_x2_matlab.shape
-diff_ratio = diff_abssum / (h * w) * 100
-print(diff_abssum, diff_ratio)
-
-# downsample x3
-img_down_x3 = imresize(img, 1 / 3, antialiasing=True)
-img_down_x3 = (img_down_x3 * 255).round()
-img_down_x3_matlab = cv2.imread('imresize_bicubic/baboon_down_x3_matlab.png')
-diff = img_down_x3 - img_down_x3_matlab
-diff_abssum = np.sum(np.abs(diff))
-h, w, _ = img_down_x3_matlab.shape
-diff_ratio = diff_abssum / (h * w) * 100
-print(diff_abssum, diff_ratio)
-
-# downsample x4
-img_down_x4 = imresize(img, 1 / 3, antialiasing=True)
-img_down_x4 = (img_down_x4 * 255).round()
-img_down_x4_matlab = cv2.imread('imresize_bicubic/baboon_down_x4_matlab.png')
-diff = img_down_x4 - img_down_x4_matlab
-diff_abssum = np.sum(np.abs(diff))
-h, w, _ = img_down_x4_matlab.shape
-diff_ratio = diff_abssum / (h * w) * 100
-print(diff_abssum, diff_ratio)
+# upsample
+for mode in ['down', 'up']:
+    for scale in [2, 3, 4]:
+        img_resize = imresize(img, 2, antialiasing=True)
+        img_resize = (img_resize * 255).round()
+        img_resize_matlab = cv2.imread(
+            f'imresize_bicubic/baboon_{mode}_x{scale}_matlab.png')
+        diff = img_resize - img_resize_matlab
+        diff_abssum = np.sum(np.abs(diff))
+        h, w, _ = img_resize_matlab.shape
+        diff_ratio = diff_abssum / (h * w) * 100
+        print(
+            f'Mode {mode} X {scale}. Diff: {diff_abssum:d}, {diff_ratio:.2f}%.'
+        )
